094daa8382da4134520afedfc4aeb93c6578053c
Tyler Kennedy <tk@tkte.ch>
2024-09-05 00:02:09 -0400

    Ensure build files are included in release packages.

--- chancy/plugins/api/ui/vite.config.ts
+++ chancy/plugins/api/ui/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   build: {
     outDir: '../dist',
     // Since our dist directory is outside the vite root,
