3b824e815d74c8ea77086a21b18dbdc27c5ddf9a
Tyler Kennedy <tk@tkte.ch>
2024-11-17 14:53:28 -0500

    Add a test for the new timeout on wait_for_job.

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -1,8 +1,13 @@
+import time
 import asyncio
 
 import pytest
 
 from chancy import Chancy, Worker, Queue, Job, QueuedJob
+
+
+def slow_job_to_run():
+    time.sleep(5)
 
 
 def job_to_run():
@@ -82,3 +87,22 @@ async def test_async_job_instance_kwarg(
 
     assert job.state == job.State.SUCCEEDED
     assert job.meta.get("received_instance") is True
+
+
+@pytest.mark.asyncio
+async def test_wait_for_job_timeout(
+    chancy: Chancy, worker: tuple[Worker, asyncio.Task], sync_executor: str
+):
+    """
+    Ensures that waiting for a job times out as expected.
+    """
+    await chancy.declare(Queue("low", executor=sync_executor))
+
+    ref = await chancy.push(Job.from_func(job_to_run, queue="low"))
+    job = await chancy.wait_for_job(ref, timeout=10)
+
+    assert job.state == job.State.SUCCEEDED
+
+    ref = await chancy.push(Job.from_func(slow_job_to_run, queue="low"))
+    with pytest.raises(asyncio.TimeoutError):
+        await chancy.wait_for_job(ref, timeout=0.1)
