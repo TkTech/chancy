625fe84359a2083d9a50d76834c76918a9f042fc
Tyler Kennedy <tk@tkte.ch>
2024-11-20 07:09:12 -0500

    Fix duplicate test added by poor merge.

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -26,10 +26,6 @@ async def async_job_with_instance(*, job: QueuedJob):
     job.meta["received_instance"] = True
 
 
-def slow_job_to_run():
-    time.sleep(5)
-
-
 @pytest.mark.asyncio
 async def test_basic_job_sync(
     chancy: Chancy, worker: tuple[Worker, asyncio.Task], sync_executor: str
@@ -110,22 +106,3 @@ async def test_async_job_instance_kwarg(
 
     assert job.state == job.State.SUCCEEDED
     assert job.meta.get("received_instance") is True
-
-
-@pytest.mark.asyncio
-async def test_wait_for_job_timeout(
-    chancy: Chancy, worker: tuple[Worker, asyncio.Task], sync_executor: str
-):
-    """
-    Ensures that waiting for a job times out as expected.
-    """
-    await chancy.declare(Queue("low", executor=sync_executor))
-
-    ref = await chancy.push(Job.from_func(job_to_run, queue="low"))
-    job = await chancy.wait_for_job(ref, timeout=10)
-
-    assert job.state == job.State.SUCCEEDED
-
-    ref = await chancy.push(Job.from_func(slow_job_to_run, queue="low"))
-    with pytest.raises(asyncio.TimeoutError):
-        await chancy.wait_for_job(ref, timeout=0.1)
