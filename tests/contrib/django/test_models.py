b2826f43138e2ec8e6b951fc06ccfe7cb1763a4f
Tyler Kennedy <tk@tkte.ch>
2025-03-23 15:52:45 -0400

    Add the Queue to admin.

--- tests/contrib/django/test_models.py
+++ tests/contrib/django/test_models.py
@@ -41,3 +41,19 @@ async def test_chancy_worker_django_query(chancy, worker):
 
     assert orm_worker.worker_id == worker.worker_id
     assert len(orm_worker.tags) > 1 and "*" in orm_worker.tags
+
+
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_chancy_queue_django_query(chancy, worker):
+    """
+    Test that we can query the Queue table.
+    """
+    from chancy.contrib.django.models import Queue
+
+    orm_queue = await Queue.objects.aget(name="default")
+
+    assert orm_queue.name == "default"
+    assert orm_queue.state == "active"
+    assert orm_queue.concurrency is None
+    assert len(orm_queue.tags) == 1
