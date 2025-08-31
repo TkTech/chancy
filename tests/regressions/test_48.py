af28d2bfbf0a10c99ca6c722a4a97a4941a1cfdf
Tyler Kennedy <tk@tkte.ch>
2025-08-31 03:53:51 -0400

    Linting.

--- tests/regressions/test_48.py
+++ tests/regressions/test_48.py
@@ -11,14 +11,20 @@ async def quick_job():
 
 
 @pytest.mark.asyncio
-# @pytest.mark.parametrize("chancy", [{"notifications": False}], indirect=True)
 async def test_regression_48(chancy: Chancy, worker: Worker):
     """
     When queues are configured with `concurrency=1`, it will always wait for
     `polling_interval` before fetching additional jobs, even if it's likely
     that there are more jobs to process.
     """
-    await chancy.declare(Queue("single", concurrency=1, polling_interval=5, executor=Chancy.Executor.Async))
+    await chancy.declare(
+        Queue(
+            "single",
+            concurrency=1,
+            polling_interval=5,
+            executor=Chancy.Executor.Async,
+        )
+    )
 
     all_refs = [
         ref
